893b0e9eadc0eff8c954485256d0247f02dfcaf6
Ren F <renfinn89@gmail.com>
2020-03-18 13:15:59 -0700

    final

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 'use strict';
-// This event should cause the program to create the FizzBuzz sequence up to the number requested by the user.
 
-// For each element in sequence insert an element into the .js-results div that looks like this:
+function fizzBuzz() {
+  $('button').click(function(event) {
+    event.preventDefault();
+    let outPut = $('input').val();
+    console.log(outPut);
 
-// <div class="fizz-buzz-item">
-//   <span>1</span>
-// </div>
-
-// If the element's content is the word "fizz", the div with .fizz-buzz-item should also get the class .fizz applied. So:
-
-// <div class="fizz-buzz-item fizz">
-//   <span>fizz</span>
-// </div>
-// If the element's content is the word "buzz" it should get the class ".buzz" applied:
-
-// <div class="fizz-buzz-item buzz">
-//   <span>buzz</span>
-// </div>
-// If its content is the word "fizzbuzz" it should get the class "fizzbuzz" applied:
-
-// <div class="fizz-buzz-item fizzbuzz">
-//   <span>fizzbuzz</span>
-// </div>
-// Your function should append the resulting HTML to the .js-results DOM element.
-
-//1st get number from form
-//2nd out put number on div
-//form =  $('number-chooser')
-//input = $('number-choice')
-$('button').click(function(event) {
-  event.preventDefault();
-  let outPut = $('input').val();
-  console.log(outPut);
-
-  //$('.js-results').append('<div class="fizz-buzz-item fizzbuzz"></div>');
-  if (outPut % 15 === 0) {
-    $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"></div>');
+    if (outPut % 15 === 0) {
+      $('.js-results').append(
+        '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'
+      );
     } else if (outPut % 5 === 0) {
-      $('.js-results').append('<div class="fizz-buzz-item buzz"></div>');
+      $('.js-results').append(
+        '<div class="fizz-buzz-item buzz"><span>buzz</span></div>'
+      );
     } else if (outPut % 3 === 0) {
-      $('.js-results').append('<div class="fizz-buzz-item fizz"></div>');
-    }else{
-        $('.js-results').append(outPut);
+      $('.js-results').append(
+        '<div class="fizz-buzz-item fizz"><span>fizz</span></div>'
+      );
+    } else {
+      $('.js-results').append(`<div class="fizz-buzz-item">
+       <span>${outPut}</span>
+     </div>`);
     }
-});
+  });
+}
+
+$(fizzBuzz);
